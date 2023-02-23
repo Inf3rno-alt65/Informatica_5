857c91735a1547846b8f847d95521a972694560c
Andrea Schiavone (AkA Inf3rno) ♊ <81051480+Inf3rno-alt65@users.noreply.github.com>
2023-02-23 18:51:33 +0100

    Add files via upload
    Login without redirect

---
+++ login2_no_redirect/login.php
@@ -0,0 +1,80 @@
+<?php
+// connessione al database
+
+try {
+    $db = new PDO("mysql:host=localhost;dbname=controllo", "root", "");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connessione al database fallita: " . $e->getMessage();
+}
+
+// verifica se il form è stato inviato
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // recupera i dati inviati dall'utente
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    // interrogazione del database
+    try {
+        $query = $db->prepare("SELECT * FROM utenti WHERE username = :username AND password = :password");
+        $query->bindParam(":username", $username);
+        $query->bindParam(":password", $password);
+        $query->execute();
+        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    } catch(PDOException $e) {
+        echo "Errore nella query: " . $e->getMessage();
+    }
+    
+    // verifica delle credenziali fornite dall'utente
+    if (count($rows) > 0) {
+        // le credenziali sono corrette, inizializza la sessione
+        session_start();
+        $_SESSION["logged_in"] = true;
+        $_SESSION["username"] = $username;
+        // reindirizza alla pagina protetta
+		$login_effettuato=true;
+        //header("Location: riservata.php");
+        //exit;
+    } else {
+        // le credenziali sono errate, visualizza un messaggio di errore
+        echo "Username o password non corretti.";
+    }
+	
+	
+}
+function show_form_login() {
+?>
+    <form action="<?=htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">	
+        <br>
+		<br>
+        <input type="submit" value="Login">
+    </form>
+<?php
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+
+if($login_effettuato){
+	echo "link riservata";
+}
+else
+{
+	show_form_login();
+}
+?>
+
+</body>
+</html>
---
+++ login2_no_redirect/riservata.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<h1>pagina riservata</h1>
+
+Login effettuato con successo!
+
+</body>
+</html>
